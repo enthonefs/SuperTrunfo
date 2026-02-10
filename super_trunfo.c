fb889fa7dac13f9eda23fc5dca406eb38aa42344
Enthone <enthoneferreira9@gmail.com>
2026-02-10 17:45:08 -0300

    Adicionando alterações de acordo com o Desafio 3

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,30 +1,50 @@
 #include <stdio.h>
 
 int main() {
-    char estado1[3] = "SP";
-    char codigo1[5] = "ABPQ";
-    char nome1[50] = "Sao Paulo";
-    long int populacao1 = 110000;
-    float area1;
-    float pib1;
-    int pontosTuristicos1;
+    char pais1[] = "Brasil";
+    char pais2[] = "Argentina";
 
-    char estado2[3] = "PE";
-    char codigo2[5] = "AQMW";
-    char nome2[50] = "Recife";
-    long int populacao2 = 20000;
-    float area2;
-    float pib2;
-    int pontosTuristicos2;
+    int pop1 = 214000000, pop2 = 46000000;
+    float area1 = 8515767, area2 = 2780400;
+    float pib1 = 1900, pib2 = 640;
+    int pontos1 = 15, pontos2 = 10;
+    float dens1 = pop1 / area1;
+    float dens2 = pop2 / area2;
 
-    if (populacao1 > populacao2) {
-    printf("%s venceu! População: %ld\n", nome1, populacao1);
-    } else if (populacao1 < populacao2) {
-    printf("%s venceu! População: %ld\n", nome2, populacao2);
-    } else {
-    printf("Empate! Ambos têm população: %ld\n", populacao1);
+    int opcao;
+
+    printf("Escolha o atributo para comparação:\n");
+    printf("1 - População\n");
+    printf("2 - Área\n");
+    printf("3 - PIB\n");
+    printf("4 - Pontos Turísticos\n");
+    printf("5 - Densidade Demográfica\n");
+    scanf("%d", &opcao);
+
+    switch (opcao) {
+        case 1:
+            printf("Comparando População:\n");
+            if (pop1 > pop2)
+                printf("%s venceu!\n", pais1);
+            else if (pop2 > pop1)
+                printf("%s venceu!\n", pais2);
+            else
+                printf("Empate!\n");
+            break;
+
+        case 5:
+            printf("Comparando Densidade Demográfica:\n");
+            if (dens1 < dens2)
+                printf("%s venceu!\n", pais1);
+            else if (dens2 < dens1)
+                printf("%s venceu!\n", pais2);
+            else
+                printf("Empate!\n");
+            break;
+
+        default:
+            printf("Opção inválida!\n");
     }
-
-
+    
     return 0;
 }
